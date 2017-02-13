17f92696e86cb4d560b15928582fc143c26d1903
alkxyly <alkxyly@hotmail.com>
2017-02-13 11:21:50 -0200

    correcao

---
+++ Aluguel/CadastrarViewController.swift
@@ -0,0 +1,47 @@
+//
+//  CadastrarViewController.swift
+//  Aluguel
+//
+//  Created by Student on 2/13/17.
+//  Copyright © 2017 Alkxyly Samyr. All rights reserved.
+//
+
+import UIKit
+
+class CadastrarViewController: UIViewController {
+    let nomes = ["Apartamento", "Kitnet","Casa"]
+    
+    @IBOutlet weak var pickerView: UIPickerView!{
+        didSet {
+            // Definimos que os métodos de Data Source e Delegate do Picker View foram implementados pela nossa classe
+            pickerView.dataSource = self
+            pickerView.delegate = self
+        }
+    }
+    
+
+}
+extension CadastrarViewController: UIPickerViewDataSource, UIPickerViewDelegate {
+    // MARK: Picker View Data Source
+    // Quantas colunas a Picker View deve ter?
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    // Quantas linhas tem em cada coluna?
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return nomes.count
+    }
+    
+    // MARK: - Picker View Delegate
+    // Qual texto vai aparecer em cada linha na coluna?
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return nomes[row]
+    }
+    
+    // O que vai acontecer quando o usuário selecionar uma coluna?
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        
+    }
+    
+}
