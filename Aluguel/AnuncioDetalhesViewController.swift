04f110785433ef04fef8c845471d7d873b5c0044
Dimitri Menezes <dimitri.menezes@gmail.com>
2017-02-14 11:38:30 -0200

    Novos Controllers , View de listagem de anuncios

---
+++ Aluguel/AnuncioDetalhesViewController.swift
@@ -0,0 +1,17 @@
+//
+//  AnuncioDetalhesViewController.swift
+//  Aluguel
+//
+//  Created by Student on 2/14/17.
+//  Copyright © 2017 Alkxyly Samyr. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class AnuncioDetalhesViewController: UIViewController{
+
+
+
+
+}
---
+++ Aluguel/AnuncioTableCell.swift
@@ -0,0 +1,24 @@
+//
+//  AnuncioTableCell.swift
+//  Aluguel
+//
+//  Created by Student on 2/13/17.
+//  Copyright © 2017 Alkxyly Samyr. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+
+
+class AnuncioTableCell: UITableViewCell {
+    
+    @IBOutlet weak var fotoImageView: UIImageView!
+    
+    @IBOutlet weak var descricaoLabel: UILabel!
+
+    @IBOutlet weak var tituloLabel: UILabel!
+  
+    @IBOutlet weak var dataLabel: UILabel!
+    @IBOutlet weak var precoLabel: UILabel!
+}
