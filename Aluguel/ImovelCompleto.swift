c739e6625ff545c5e2f664f48101b1d8417aa9a4
alkxyly <alkxyly@hotmail.com>
2017-02-14 10:42:08 -0200

    aluguel quarto

--- Aluguel/ImovelCompleto.swift
+++ Aluguel/ImovelCompleto.swift
@@ -10,23 +10,6 @@ import Foundation
 
 class ImovelCompleto:Imovel {
     
-    var qntQuartos:Int?
-    var qntSuites: Int?
-    var qntVagasGaragem:Int?
-    
-    init(qntQuartos: Int, qntSuites: Int, qntVagasGaragem: Int) {
-        self.qntQuartos = qntQuartos
-        self.qntSuites = qntSuites
-        self.qntVagasGaragem = qntVagasGaragem
-    }
-    
-    init(tipoImovel: String, Endereco:Endereco, dono:Usuario, valorAluguel:Float , qntQuartos:Int, qntSuites:Int, qntVagasGaragem:Int) {
-        // redirect to superclass Imovel
-        super.init(tipoDoImovel: <#T##String#>, endereco: <#T##Endereco#>, dono: <#T##Usuario#>, valorAluguel: <#T##Float#>)
-        // initialization atributes ImovelCompleto
-        self.qntQuartos = qntQuartos
-        self.qntSuites = qntSuites
-        self.qntVagasGaragem = qntVagasGaragem
-    }
+
     
 }
--- Aluguel/ImovelQuarto.swift
+++ Aluguel/ImovelQuarto.swift
@@ -10,22 +10,6 @@ import Foundation
 
 class ImovelQuarto: Imovel {
     
-    var sexo: String?
-    var descricaoPessoal: String?
-    
-    init(tipoDoImovel: String, endereco: Endereco, dono: Usuario, valorAluguel: Float,
-         sexo: String, descricaoPessoal: String) {
-        // redirect to superclass Imovel
-        super.init(tipoDoImovel: <#T##String#>, endereco: <#T##Endereco#>, dono: <#T##Usuario#>, valorAluguel: <#T##Float#>)
-        // initialization atributes ImovelQuarto
-        self.sexo = sexo
-        self.descricaoPessoal = descricaoPessoal
-    }
-    
-    init(sexo: String, descricaoPessoal: String) {
-        // initialization atributes ImovelQuarto
-        self.sexo = sexo
-        self.descricaoPessoal = descricaoPessoal
-    }
+
 
 }
